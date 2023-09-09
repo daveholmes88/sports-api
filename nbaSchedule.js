dccc90bdbd8925f9eef10670108430f3bf1c135a
Dave Holmes <daveholmes88@gmail.com>
2023-09-09 12:45:16 -0500

    cleanup

--- nbaSchedule.js
+++ nbaSchedule.js
@@ -15,24 +15,6 @@ const handler = async () => {
             games.push([homeTeam, homeScore, awayTeam, awayScore])
         })
     }
-    //     const gameIds = []
-    //     const schedule = weekData.content.schedule
-    //     // console.log(schedule)
-    //     const dates = Object.keys(schedule)
-    //     dates.forEach(date => {
-    //         schedule[date].games.forEach(game => gameIds.push(game.id))
-    //     })
-    //     for (const id of gameIds) {
-    //         const jsonGame = await fetch(`https://cdn.espn.com/core/nfl/boxscore?xhr=1&gameId=${id}`)
-    //         const gameData = await jsonGame.json()
-    //         const homeTeam = gameData.__gamepackage__.homeTeam.team.name;
-    //         const homeScore = gameData.__gamepackage__.homeTeam.score;
-    //         const awayTeam = gameData.__gamepackage__.awayTeam.team.name;
-    //         const awayScore = gameData.__gamepackage__.awayTeam.score;
-    //         games.push([homeTeam, homeScore, awayTeam, awayScore])
-    //         // games += `${homeTeam} ${homeScore} ${awayTeam} ${awayScore}` + "\r\n";
-    //     }  
-    // }
     const csvFromGames = convertArrayToCSV(games, {
         header,
         separator: ','
