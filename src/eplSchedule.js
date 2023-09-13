8bbf1e2bc8c66b3442b12d8a0e24dd3b991f3726
Dave Holmes <daveholmes88@gmail.com>
2023-09-13 15:19:13 -0500

    prettier yaml

--- src/eplSchedule.js
+++ src/eplSchedule.js
@@ -1,39 +1,39 @@
-const fs = require("fs");
-const { convertArrayToCSV } = require("convert-array-to-csv");
+const fs = require('fs');
+const { convertArrayToCSV } = require('convert-array-to-csv');
 
 const handler = async () => {
-  const token = process.env.FOOTBALL_DATA_TOKEN;
-  console.log(token);
-  const header = [["Home Team", "HS", "Away Team", "AS"]];
-  let games = [];
-  for (let i = 1; i < 39; i++) {
-    await new Promise((resolve) => setTimeout(resolve, 11000));
-    const json = await fetch(
-      `https://api.football-data.org/v4/competitions/PL/matches?status=FINISHED&season=2022&matchday=${i}`,
-      {
-        headers: {
-          "X-Auth-Token": token,
-        },
-      },
-    );
-    const data = await json.json();
-    console.log(data);
-    data.matches.forEach((d) => {
-      const homeTeam = d.homeTeam.name;
-      const homeScore = d.score.fullTime.home;
-      const awayTeam = d.awayTeam.name;
-      const awayScore = d.score.fullTime.away;
-      games.push([homeTeam, homeScore, awayTeam, awayScore]);
+    const token = process.env.FOOTBALL_DATA_TOKEN;
+    console.log(token);
+    const header = [['Home Team', 'HS', 'Away Team', 'AS']];
+    let games = [];
+    for (let i = 1; i < 39; i++) {
+        await new Promise(resolve => setTimeout(resolve, 11000));
+        const json = await fetch(
+            `https://api.football-data.org/v4/competitions/PL/matches?status=FINISHED&season=2022&matchday=${i}`,
+            {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+            }
+        );
+        const data = await json.json();
+        console.log(data);
+        data.matches.forEach(d => {
+            const homeTeam = d.homeTeam.name;
+            const homeScore = d.score.fullTime.home;
+            const awayTeam = d.awayTeam.name;
+            const awayScore = d.score.fullTime.away;
+            games.push([homeTeam, homeScore, awayTeam, awayScore]);
+        });
+    }
+    const csvFromGames = convertArrayToCSV(games, {
+        header,
+        separator: ',',
     });
-  }
-  const csvFromGames = convertArrayToCSV(games, {
-    header,
-    separator: ",",
-  });
-  fs.writeFile("./csv/eplSchedule.csv", csvFromGames, (err) => {
-    if (err) console.log(err);
-    else console.log("csv file written");
-  });
+    fs.writeFile('./csv/eplSchedule.csv', csvFromGames, err => {
+        if (err) console.log(err);
+        else console.log('csv file written');
+    });
 };
 
 handler();
--- src/nbaSchedule.js
+++ src/nbaSchedule.js
@@ -1,30 +1,30 @@
-const fs = require("fs");
-const { convertArrayToCSV } = require("convert-array-to-csv");
+const fs = require('fs');
+const { convertArrayToCSV } = require('convert-array-to-csv');
 
 const handler = async () => {
-  const header = [["Home Team", "HS", "Away Team", "AS"]];
-  let games = [];
-  for (let i = 0; i < 15; i++) {
-    const json = await fetch(
-      `https://www.balldontlie.io/api/v1/games?start_date=%272022-10-01&end_date=2023-07-01&per_page=100&page=${i}`,
-    );
-    const data = await json.json();
-    data.data.forEach((d) => {
-      const homeTeam = d.home_team.full_name;
-      const homeScore = d.home_team_score;
-      const awayTeam = d.visitor_team.full_name;
-      const awayScore = d.visitor_team_score;
-      games.push([homeTeam, homeScore, awayTeam, awayScore]);
+    const header = [['Home Team', 'HS', 'Away Team', 'AS']];
+    let games = [];
+    for (let i = 0; i < 15; i++) {
+        const json = await fetch(
+            `https://www.balldontlie.io/api/v1/games?start_date=%272022-10-01&end_date=2023-07-01&per_page=100&page=${i}`
+        );
+        const data = await json.json();
+        data.data.forEach(d => {
+            const homeTeam = d.home_team.full_name;
+            const homeScore = d.home_team_score;
+            const awayTeam = d.visitor_team.full_name;
+            const awayScore = d.visitor_team_score;
+            games.push([homeTeam, homeScore, awayTeam, awayScore]);
+        });
+    }
+    const csvFromGames = convertArrayToCSV(games, {
+        header,
+        separator: ',',
     });
-  }
-  const csvFromGames = convertArrayToCSV(games, {
-    header,
-    separator: ",",
-  });
-  fs.writeFile("./csv/nbaSchedule.csv", csvFromGames, (err) => {
-    if (err) console.log(err);
-    else console.log("csv file written");
-  });
+    fs.writeFile('./csv/nbaSchedule.csv', csvFromGames, err => {
+        if (err) console.log(err);
+        else console.log('csv file written');
+    });
 };
 
 handler();
