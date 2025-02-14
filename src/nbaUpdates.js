59c711de79f1007d5a8b05897043f1a44c57de04
Dave Holmes <daveholmes88@gmail.com>
2025-02-14 14:15:23 -0600

    update data

--- src/nbaUpdates.js
+++ src/nbaUpdates.js
@@ -2,7 +2,7 @@ const pkg = require('pg');
 const fs = require('fs');
 
 const { Pool } = pkg
-const DATE = '20250212'
+const DATE = '20250213'
 const PASSWORD = process.env.PASSWORD;
 
 const pool = new Pool({
