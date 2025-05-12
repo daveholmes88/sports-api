55d10cdb839d9aa23ef09ce36f510412aaf732dc
Dave Holmes <daveholmes88@gmail.com>
2025-05-12 14:25:29 -0500

    wnba model and updates created

---
+++ src/wnbaUpdates.js
@@ -0,0 +1,108 @@
+const pkg = require('pg');
+const fs = require('fs');
+
+const { Pool } = pkg
+const DATE = '20240518'
+const PASSWORD = process.env.PASSWORD;
+
+const pool = new Pool({
+  user: 'davidholmes',
+  database: 'backendgambling_development',
+  password: PASSWORD,
+  port: 5432, // This is the default PostgreSQL port
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+});
+
+const getLastNightGames = async () => {
+    const jsonLastNight = await fetch(
+        `https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates=${DATE}`
+    );
+    const lastNightData = await jsonLastNight.json();
+    const lastNightGames = [];
+    for (const game of lastNightData.events) {
+        const gameArray = game.name.split(' at ');
+        const gameObj = {};
+        gameObj['homeTeam'] = gameArray[1];
+        gameObj['awayTeam'] = gameArray[0];
+        gameObj['date'] = game.date.split('T')[0];
+        gameObj['id'] = parseInt(game.id)
+        game.competitions[0].competitors.forEach(c => {
+            if (c['homeAway'] === 'home') {
+                gameObj['homeScore'] = parseInt(c.score);
+                gameObj['homeHalfScore'] = parseInt(c.linescores[0].value) + parseInt(c.linescores[1].value);
+            } else {
+                gameObj['awayScore'] = parseInt(c.score);
+                gameObj['awayHalfScore'] = parseInt(c.linescores[0].value) + parseInt(c.linescores[1].value);
+            }
+        });
+        lastNightGames.push(gameObj);
+    }
+    return lastNightGames
+}
+
+const rounded = num => Math.round(num * 100) / 100;
+
+const saveOldInfo = (teams) => {
+    const jsonString = JSON.stringify(teams)
+    fs.writeFile(`./csv/wnbaDB/WNBA${DATE}.json`, jsonString, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('JSON file has been created successfully!');
+    });
+    fs.writeFile(`./src/wnbaData.json`, jsonString, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('JSON file has been created successfully!');
+    });
+}
+
+const updateInfo = async () => {
+    const games = await getLastNightGames()
+    const client = await pool.connect();
+    const resultTeams = await pool.query('SELECT * FROM wnba_teams;')
+    const resultGames = await pool.query('SELECT * FROM wnba_games;')
+    const teams = resultTeams.rows
+    const savedGames = resultGames.rows
+    const newGames = games.filter(game => {
+        const g = savedGames.find(sg => sg.id === game.id)
+        return (!g)
+    })
+    if (games.length === newGames.length) {
+        saveOldInfo(teams)
+    }
+    console.log(teams)
+    for (let g of newGames) {
+        if (g.homeScore !== 0 & g.awayScore !== 0) {
+            const netScore = g.homeScore - g.awayScore;
+            const total = g.homeScore + g.awayScore;
+            const halfTotal = g.homeHalfScore + g.awayHalfScore;
+            const halfScore = g.homeHalfScore - g.awayHalfScore;
+            const homeTeam = teams.find(t => t.name === g.homeTeam);
+            const awayTeam = teams.find(t => t.name === g.awayTeam);
+            homeTeam.rating = rounded((netScore + awayTeam.rating)*0.1 + (homeTeam.rating * 0.9))
+            awayTeam.rating = rounded((netScore * -1 + homeTeam.rating)*0.1 + (awayTeam.rating * 0.9))
+            homeTeam.half = rounded((halfScore + awayTeam.half)*0.1 + (homeTeam.half * 0.9))
+            awayTeam.half = rounded((halfScore * -1 + homeTeam.rating)*0.1 + (awayTeam.rating * 0.9))
+            homeTeam.total = rounded((total*0.1) + (homeTeam.total*0.9))
+            awayTeam.total = rounded((total*0.1) + (awayTeam.total*0.9))
+            homeTeam.total = rounded((halfTotal*0.1) + (homeTeam.half_total*0.9))
+            awayTeam.total = rounded((halfTotal*0.1) + (awayTeam.half_total*0.9))
+            await pool.query(`INSERT INTO wnba_games (id, home_team, away_team, home_score, away_score, home_half_score, away_half_score, date)
+                VALUES (${g.id}, '${g.homeTeam}', '${g.awayTeam}', ${g.homeScore}, ${g.awayScore}, ${g.homeHalfScore}, ${g.awayHalfScore}, '${g.date}')`)
+        }
+    }
+    console.log(teams)
+    for (let team of teams) {
+        await pool.query(`UPDATE wnba_teams 
+            SET rating = ${team.rating}, half=${team.half}
+            WHERE id = ${team.id};`)
+    }
+    client.release();
+    pool.end();
+} 
+
+updateInfo()
---
+++ src/wnbamodel.js
@@ -0,0 +1,183 @@
+const fs = require('fs');
+const { convertArrayToCSV } = require('convert-array-to-csv');
+const pkg = require('pg');
+require('dotenv').config();
+
+const { Pool } = pkg;
+const PASSWORD = process.env.PASSWORD;
+
+const pool = new Pool({
+    user: 'davidholmes',
+    database: 'backendgambling_development',
+    password: PASSWORD,
+    port: 5432, // This is the default PostgreSQL port
+    //   ssl: {
+    //     rejectUnauthorized: false
+    //   }
+});
+
+const homeFieldAdvantage = {
+    AtlantaDream: 10,
+    ChicagoSky: 10, 
+    ConnecticutSun: 10,
+    DallasWings: 10,
+    GoldenStateValkyries: 10,
+    IndianaFever: 10,
+    LasVegasAces: 10,
+    LosAngelesSparks: 10,
+    MinnesotaLynx: 10,
+    NewYorkLiberty: 10,
+    PhoenixMercury:10,
+    SeattleStorm: 10,
+    WashingtonMystics: 10,
+};
+
+const checkHomeField = game => {
+    const { neutral, home } = game;
+    if (neutral) return 0;
+    return homeFieldAdvantage[home.replaceAll(' ', '')];
+};
+
+const rounding = num => Math.round(num * 100) / 100;
+
+const getDateGameInfo = async (date, lastWeek, daysAgo) => {
+    date.setDate(date.getDate() - 1);
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const newDate = `${year}${month}${day}`;
+    const jsonGames = await fetch(
+        `https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates=${newDate}`
+    );
+    played = [];
+    const games = await jsonGames.json();
+    if (games.events.length > 0) {
+        games.events.forEach(g => {
+            const gameArray = g.name.split(' at ');
+            const home = gameArray[1];
+            const away = gameArray[0];
+            const overtime = g.competitions[0].status.period === 5;
+            played.push({ home, away, overtime });
+        });
+    }
+    lastWeek[daysAgo] = played;
+};
+
+const checkPlayedYesterday = (game, yesterdayGames) => {
+    const { away, home } = game;
+    let impact = 0;
+    const awayPlayed = yesterdayGames.find(
+        y => y.home === away || y.away === away
+    );
+    const homePlayed = yesterdayGames.find(
+        y => y.home === home || y.away === home
+    );
+    if (awayPlayed) impact = 10;
+    if (homePlayed) impact = -10;
+    return impact;
+};
+const checkPlayedLastWeek = (game, lastWeek) => {
+    const { away, home } = game;
+    let impact = 0;
+    let awayPlayed = 0;
+    let homePlayed = 0;
+    for (let i = 1; i < 7; i++) {
+        const filteredAway = lastWeek[i].find(
+            y => y.home === away || y.away === away
+        );
+        const filteredHome = lastWeek[i].find(
+            y => y.home === home || y.away === home
+        );
+        if (filteredAway) awayPlayed += 2;
+        if (filteredHome) homePlayed += 2;
+        if (i === 3) {
+            if (awayPlayed > 1) impact += 5;
+            if (homePlayed > 1) impact -= 5;
+        }
+        if (i === 5) {
+            if (awayPlayed > 3) impact += 5;
+            if (homePlayed > 3) impact -= 5;
+        }
+    }
+    if (awayPlayed > 4) impact += 5;
+    if (homePlayed > 4) impact -= 5;
+    return impact;
+};
+
+const handler = async () => {
+    const d = new Date();
+    // const date = d.toISOString().slice(0, 10).replace(/-/g, '');
+    const date = '20250518'
+    const client = await pool.connect();
+    const result = await pool.query(`SELECT * FROM wnba_teams`);
+    const teams = result.rows;
+    client.release();
+    pool.end();
+    const jsonGames = await fetch(
+        `https://site.api.espn.com/apis/site/v2/sports/basketball/wnba/scoreboard?dates=${date}`
+    );
+    const games = await jsonGames.json();
+    lastWeek = {};
+    for (let i = 1; i < 7; i++) {
+        await getDateGameInfo(d, lastWeek, i);
+    }
+    const todayGames = [];
+    games.events.forEach(game => {
+        let odds = 'n/a';
+        if (game.competitions[0]?.odds) {
+            odds = game.competitions[0]?.odds[0].details;
+        }
+        const gameArray = game.name.split(' at ');
+        todayGames.push({
+            away: gameArray[0],
+            home: gameArray[1],
+            odds,
+            neutral: game.competitions[0].neutralSite,
+            id: game.id,
+        });
+    });
+    const final = todayGames.map(game => {
+        const { odds, home, away } = game;
+        const awayTeam = teams.find(team => away === team.name);
+        const homeTeam = teams.find(team => home === team.name);
+        const dbRating = homeTeam.rating - awayTeam.rating;
+        let spread = 0;
+        const homeField = checkHomeField(game);
+        spread += homeField;
+        const yesterdayCheck = checkPlayedYesterday(game, lastWeek[1]);
+        spread += yesterdayCheck;
+        const lastWeekGames = checkPlayedLastWeek(game, lastWeek);
+        spread += lastWeekGames;
+        spread = spread / 5;
+        let homeSpread = rounding(dbRating + spread) * -1;
+        homeSpread = homeSpread > 0 ? `+${homeSpread}` : homeSpread;
+        return [
+            home,
+            homeTeam.rating,
+            away,
+            awayTeam.rating,
+            spread,
+            `${home} ${homeSpread}`,
+            odds,
+        ];
+    });
+    const header = [
+        'Home Team',
+        'Home DB Rating',
+        'Away Team',
+        'Away DB Rating',
+        'Environmental Factors',
+        'DB Spread',
+        'ESPN Spread',
+    ];
+    const csvFromGames = convertArrayToCSV(final, {
+        header,
+        separator: ',',
+    });
+    fs.writeFile(`./csv/wnbaModel/wnbaModel${date}.csv`, csvFromGames, err => {
+        if (err) console.log(err);
+        else console.log('model csv file written');
+    });
+};
+
+handler();
